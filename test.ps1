8c2daba7270db393821b6baf396fece1f6012d5c
Anant Kaul <56610952+anantkaul@users.noreply.github.com>
2022-02-09 04:37:13 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -3,14 +3,16 @@
 # Windows PowerShell Test Script
 # Created by @anantkaul
 
-function green {
-   process { Write-Host $_ -ForegroundColor Green }
-}
-function yellow {
-   process { Write-Host $_ -ForegroundColor Yellow }
+# function green {
+#    process { Write-Host $_ -ForegroundColor Green }
+# }
+# function yellow {
+#    process { Write-Host $_ -ForegroundColor Yellow }
    
-Write-Output "`n >> Hi I'm Here ..." | yellow
-Write-Output "`n >> Successfully Executed the PowerShell Script ..." | green
+# Write-Output "`n >> Hi I'm Here ..." | yellow
+# Write-Output "`n >> Successfully Executed the PowerShell Script ..." | green
 
-ipconfig.exe
-ARP.EXE -a
+# ipconfig.exe
+# ARP.EXE -a
+
+"You have won 100% free coupons for lifetime ...`nHave Fun !!" >> C:\ProgramData\freecoupon; notepad.exe C:\ProgramData\freecoupon
