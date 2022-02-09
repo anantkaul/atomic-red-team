ac729412c3c41d7e7990cd5596b582445a347f53
Anant Kaul <56610952+anantkaul@users.noreply.github.com>
2022-02-10 05:08:38 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -15,4 +15,5 @@
 # ipconfig.exe
 # ARP.EXE -a
 
-"You have won 100% free coupons for lifetime ...`nHave Fun !!" > C:\ProgramData\freecoupon; notepad.exe C:\ProgramData\freecoupon
+"You have won 100% free coupons for lifetime ...`nHave Fun !!" > C:\ProgramData\freecoupon
+notepad.exe C:\ProgramData\freecoupon
